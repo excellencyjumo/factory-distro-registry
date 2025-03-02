6129958639109e337e72043eadb13678680da07d
Adedamola Adejumo <57894056+excellencyjumo@users.noreply.github.com>
2025-03-02 22:15:03 +0100

    Update catchAsync.js

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,9 +1,3 @@
-// module.exports = fn => {
-//     return (req, res, next) => {
-//       fn(req, res, next).catch(next);
-//     };
-//   };
-
 const catchAsync = (fn) => {
   return (req, res, next) => {
       Promise.resolve(fn(req, res, next)).catch(next);
